addf53d3cda1353deef1dd85391cbe0ede9c73b2
Qu5antum <erae88959@gmail.com>
2026-01-17 15:19:03 +0300

    lesson update

--- backend/src/api/routes/create_course_route.py
+++ backend/src/api/routes/create_course_route.py
@@ -3,8 +3,8 @@ from backend.src.database.db import AsyncSession, get_session
 from backend.src.api.dependency import get_current_user
 from backend.src.service.course import create_new_course, update_course_by_course_id
 from backend.src.service.section import create_new_section_by_course, update_section_by_section_id
-from backend.src.service.lesson import create_new_lesson_by_section
-from backend.src.api.schemas import CourseCreate, SectionCreate, LessonCreate, CourseUpdate
+from backend.src.service.lesson import create_new_lesson_by_section, update_lesson_by_lesson_id
+from backend.src.api.schemas import CourseCreate, SectionCreate, LessonCreate, CourseUpdate, SectionUpdate, LessonUpdate
 from backend.src.database.models import User
 
 
@@ -17,17 +17,17 @@ router = APIRouter(
 @router.post("/courses", status_code=status.HTTP_201_CREATED)
 async def create_course(
     data: CourseCreate,
-    author: User = Depends(get_current_user),
+    user: User = Depends(get_current_user),
     session: AsyncSession = Depends(get_session)
 ):
     return await create_new_course(
         session=session, 
-        author=author, 
+        author=user, 
         data=data
     )
 
 
-@router.post("/courses/{course_id}", status_code=status.HTTP_201_CREATED)
+@router.put("/courses/{course_id}", status_code=status.HTTP_201_CREATED)
 async def update_course(
     course_id: int,
     data: CourseUpdate,
@@ -41,30 +41,49 @@ async def update_course(
 async def create_section(
     course_id: int,
     data: SectionCreate,
-    author: User = Depends(get_current_user),
+    user: User = Depends(get_current_user),
     session: AsyncSession = Depends(get_session)
 ):
     return await create_new_section_by_course(
         session=session, 
         course_id=course_id, 
         data=data, 
-        author=author
+        author=user
     )
+
+
+@router.put("/sections/{section_id}", status_code=status.HTTP_201_CREATED)
+async def update_section(
+    section_id: int,
+    data: SectionUpdate,
+    user: User = Depends(get_current_user),
+    session: AsyncSession = Depends(get_session)
+):
+    return await update_section_by_section_id(session=session, section_id=section_id, data=data, author=user)
+
 
 
 @router.post("/sections/{section_id}/lessons", status_code=status.HTTP_201_CREATED)
 async def create_lesson(
     section_id: int,
     data: LessonCreate,
-    author: User = Depends(get_current_user),
+    user: User = Depends(get_current_user),
     session: AsyncSession = Depends(get_session)
 ):
     return await create_new_lesson_by_section(
         session=session,
         section_id=section_id,
         data=data,
-        author=author
+        author=user
     )
 
+@router.put("/lessons/{lesson_id}", status_code=status.HTTP_201_CREATED)
+async def update_lesson(
+    lesson_id: int,
+    data: LessonUpdate,
+    user: User = Depends(get_current_user),
+    session: AsyncSession = Depends(get_session)
+):
+    return await update_lesson_by_lesson_id(session=session, lesson_id=lesson_id, data=data, author=user)
 
     
--- backend/src/service/lesson.py
+++ backend/src/service/lesson.py
@@ -25,7 +25,6 @@ async def create_new_lesson_by_section(
             detail="Bölüm bulanamadı."
         )
 
-    
     if section.course.author_id != author.id:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -42,3 +41,45 @@ async def create_new_lesson_by_section(
     await session.refresh(lesson)
 
     return lesson
+
+
+async def update_lesson_by_lesson_id(
+        session: AsyncSession,
+        lesson_id: int,
+        data: str,
+        author: User
+):
+    result = await session.execute(
+        select(Lesson)
+        .options(
+            selectinload(Lesson.section)
+            .selectinload(Section.course)
+        )
+        .where(Lesson.id == lesson_id)
+    )
+
+    lesson = result.scalar_one_or_none()
+
+    if not lesson: 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Ders bulanamadı."
+        )
+    
+    if lesson.section.course.author_id != author.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Yasaklı."
+        )
+    
+    for field, value in data.model_dump(exclude_unset=True).items():
+        setattr(lesson, field, value)
+
+    await session.commit()
+    await session.refresh(lesson)
+
+
+    return {"detail": "Değişiklikler kaydedildi"}
+    
+
+    
